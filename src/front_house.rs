8c5fb98dc98bbf63fe43e44e741223d2849c9c85
Clivern <hello@clivern.com>
2020-01-08 20:55:13 +0100

    inline test

--- src/front_house.rs
+++ src/front_house.rs
@@ -3,3 +3,11 @@ pub mod another_mod {
         "Hello".to_string()
     }
 }
+
+#[test]
+fn test_another_pub_fun() {
+    assert_eq!(
+        another_mod::another_pub_fun(),
+        "Hello".to_string()
+    );
+}
